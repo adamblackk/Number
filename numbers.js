a050707c80c6a3e8eb20e9a995c163e4d10ba51c
Adem <mr.admblck@gmail.com>
2023-12-26 16:14:28 +0300

    first commit

---
+++ numbers.js
@@ -0,0 +1,67 @@
+const express=require('express')
+
+const app=express()
+
+app.listen(300)
+
+app.get('/numbers', (req, res) => {
+
+    let numbersArray = [];
+    let request1 = req.query
+    let rqKeys1 = Object.keys(request1)[1]
+    let numberRq=request1[rqKeys1]
+    // 1'den 100'e kadar olan sayıları diziye ekle
+    for (var i = 1; i <= numberRq; i++) {
+        numbersArray.push(i);
+    }
+    
+    //console.log(numbersArray)
+    // İstek içerisindeki sayıları içeren dizi
+   
+    let request2 = req.query
+    let rqKeys2 = Object.keys(request2)[0]
+    console.log(request2[rqKeys1])
+    if(rqKeys2 == 'b'){
+        numbersArray = numbersArray.filter(item => item > Number(request2[rqKeys2]))
+    }
+    else if(rqKeys2 == 'k'){
+        numbersArray = numbersArray.filter(item => item < Number(request2[rqKeys2]))
+    }
+    else{
+        //numbersArray = Object.keys(request2).length > 0 ? [] : numbersArray
+    }
+    console.log(rqKeys1)
+    
+    res.send({
+        numbers: numbersArray
+    })
+})
+
+    //-------------bu kodlar çalışıyor--------------
+/* app.get('/numbers', (req, res) => {
+
+    let numbersArray = [];
+
+    // 1'den 100'e kadar olan sayıları diziye ekle
+    for (var i = 1; i <= 100; i++) {
+        numbersArray.push(i);
+    }
+    // İstek içerisindeki sayıları içeren dizi
+    let request = req.query
+    let rqKeys = Object.keys(request)[0]
+    if(rqKeys == 'b'){
+        numbersArray = numbersArray.filter(item => item > Number(request[rqKeys]))
+    }
+    else if(rqKeys == 'k'){
+        numbersArray = numbersArray.filter(item => item < Number(request[rqKeys]))
+    }
+    else{
+        numbersArray = Object.keys(request).length > 0 ? [] : numbersArray
+    }
+    console.log(rqKeys)
+    
+    res.send({
+        numbers: numbersArray
+    })
+}) */
+
